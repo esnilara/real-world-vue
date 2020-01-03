20a0f6f98170ebef90047ded64fdef1fcb10b717
Esteban Lara <esteban@esnilara.io>
2020-01-03 15:57:36 -0600

    Create EventCard component

--- src/router/index.js
+++ src/router/index.js
@@ -11,8 +11,9 @@ const routes = [
     component: EventList
   },
   {
-    path: '/event',
+    path: '/event/:id',
     name: 'event-show',
+    props: true,
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
@@ -28,6 +29,7 @@ const routes = [
 ];
 
 const router = new VueRouter({
+  mode: 'history',
   routes
 });
 
