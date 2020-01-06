7cd0543a66c9b3ed5870f9a7d56fb5c3b463b87f
Esteban Lara <esteban@esnilara.io>
2020-01-06 10:09:25 -0600

    Use axios to load event card data

--- src/views/EventList.vue
+++ src/views/EventList.vue
@@ -2,16 +2,32 @@
   <div>
     <h1>Events Listing</h1>
 
-    <EventCard />
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
   </div>
 </template>
 
 <script>
 import EventCard from '@/components/EventCard.vue';
+import axios from 'axios';
 
 export default {
   components: {
     EventCard
+  },
+
+  data() {
+    return {
+      events: []
+    };
+  },
+
+  async created() {
+    try {
+      const response = await axios.get('http://localhost:3000/events');
+      this.events = response.data;
+    } catch (error) {
+      console.log(`There was an error: ${error.response}`);
+    }
   }
 };
 </script>
